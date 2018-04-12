fae3c9bc2e643ea64bc77c19da4a9066636ff56a
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-04-11 20:23:25 -0500

    Import MessagesService and FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TerminalComponent } from './terminal/terminal.component';
 import { DisplayComponent } from './display/display.component';
 
+import { MessagesService } from './messages.service';
 
 @NgModule({
   declarations: [
@@ -14,9 +15,12 @@ import { DisplayComponent } from './display/display.component';
     DisplayComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    MessagesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
